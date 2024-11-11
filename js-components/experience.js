e0174cfbf99b04657d63cabbcbbbc53edf722edf
Ipek <ozcanir@ornl.gov>
2024-11-10 21:49:51 -0500

    experience images added

---
+++ js-components/experience.js
@@ -0,0 +1,5 @@
+const oakRidgePhoto = document.getElementById('oak-ridge-photo');
+const bethIsraelPhoto = document.getElementById('beth-israel-photo');
+const elementalPhoto = document.getElementById('elemental-photo');
+
+oakRidgePhoto.addEventListener("click", "https://www.ornl.gov/")
